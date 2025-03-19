4f79fd604562df89d92d314fa40cd61e941b5558
abdelmalek-lamkadem <abdelmalek.lamkadem.23@ump.ac.ma>
2025-03-19 22:52:19 +0000

    add add device forum

--- lib/control_devices.dart
+++ lib/control_devices.dart
@@ -86,7 +86,47 @@ class AddDevicePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("إضافة جهاز جديد")),
-      body: Center(child: Text("صفحة إضافة جهاز")),
+      body: Column(children: [Text("data"), NewDeviceForm()]),
+    );
+  }
+}
+
+class NewDeviceForm extends StatelessWidget {
+  const NewDeviceForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter a search term',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter your username',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter your username',
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
