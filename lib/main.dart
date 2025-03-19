4f4f7a84c4a824afd887690ea8d1386415bbc23e
abdelmalek-lamkadem <abdelmalek.lamkadem.23@ump.ac.ma>
2025-03-19 18:16:01 +0000

    add a storage of the previous state (on or off)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_background_messenger/flutter_background_messenger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import 'control_devices.dart';
 
 final messenger = FlutterBackgroundMessenger();
 
@@ -144,6 +145,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   bool isPowerOn = false;
+
   String controlAssetPowerOn = "assets/power_animation.json";
   String controlAssetPowerOff = "assets/power_off.json";
 
@@ -185,21 +187,38 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               Center(child: Header()),
               SizedBox(height: 20),
+              DeviceCard(),
+              SizedBox(height: 20),
+
+              SignalIndicator(),
+              SizedBox(height: 20),
+
               // Animated power button
-              GestureDetector(
-                onTap: _togglePower,
-                child: Lottie.asset(
-                  isPowerOn ? controlAssetPowerOff : controlAssetPowerOn,
-                  height: 150,
-                  width: 150,
-                  fit: BoxFit.cover,
+              Center(
+                child: GestureDetector(
+                  // onTapDown: _addActivity(
+                  //   "String title",
+                  //   "String subtitle",
+                  //   "IconData icon",
+                  //   "Color color",
+                  // ),
+                  onTap: _togglePower,
+                  child: Lottie.asset(
+                    isPowerOn ? controlAssetPowerOff : controlAssetPowerOn,
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
               // Status text
-              Text(
-                isPowerOn ? "إيقاف التشغيل" : "تشغيل",
-                textDirection: TextDirection.rtl,
+              Center(
+                child: Text(
+                  isPowerOn ? "إيقاف التشغيل" : "تشغيل",
+                  textDirection: TextDirection.rtl,
+                ),
               ),
+              RecentActivity(),
             ],
           ),
         ),
@@ -220,97 +239,6 @@ class Header extends StatelessWidget {
         fontSize: 24,
         fontWeight: FontWeight.bold,
       ),
-    );
-  }
-}
-
-class DeviceCard extends StatefulWidget {
-  const DeviceCard({super.key});
-
-  @override
-  _DeviceCardState createState() => _DeviceCardState();
-}
-
-class _DeviceCardState extends State<DeviceCard> {
-  bool _isExpanded = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 0, // Adds a soft shadow effect
-      color: Colors.white,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      margin: EdgeInsets.symmetric(horizontal: 6, vertical: 8),
-      child: Theme(
-        data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-        child: ExpansionTile(
-          onExpansionChanged: (expanded) {
-            setState(() {
-              _isExpanded = expanded;
-            });
-          },
-          tilePadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-          childrenPadding: EdgeInsets.only(bottom: 12),
-          iconColor: Colors.green[700],
-          collapsedIconColor: Colors.green[700],
-          trailing: AnimatedRotation(
-            turns: _isExpanded ? 0.5 : 0, // Smooth icon rotation
-            duration: Duration(milliseconds: 300),
-            child: Icon(Icons.expand_more),
-          ),
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "RTU5024",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 4),
-              Text(
-                "بركان، سيدي سليم الشرا",
-                style: TextStyle(color: Colors.grey[600], fontSize: 14),
-              ),
-            ],
-          ),
-          children: [
-            Divider(
-              indent: 16,
-              endIndent: 16,
-              thickness: 1,
-            ), // Stylish separator
-            ListTile(
-              leading: Icon(Icons.add, color: Colors.green[700]),
-              title: Text(
-                "إضافة جهاز جديد",
-                style: TextStyle(fontWeight: FontWeight.w500),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AddDevicePage()),
-                );
-              },
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              tileColor: Colors.green[50], // Subtle background color
-              splashColor: Colors.green[100], // Interactive ripple effect
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class AddDevicePage extends StatelessWidget {
-  const AddDevicePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("إضافة جهاز جديد")),
-      body: Center(child: Text("صفحة إضافة جهاز")),
     );
   }
 }
