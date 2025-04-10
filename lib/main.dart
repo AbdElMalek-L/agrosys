4e0dbe8242cbfeb3859910bd9009a9477b5a6f42
kabiriEl <elmahdikabiri2030@gmail.com>
2025-04-10 06:29:53 +0200

    first commit for KabiriEl

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:agrosys/domain/repository/device_repo.dart';
 import 'package:agrosys/presentation/pages/intro_page.dart';
 import 'package:agrosys/presentation/cubits/app_state_cubit.dart';
 
+// none
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
